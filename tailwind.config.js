a0e8d0c9b7fa0c213540cfde5cbf32df40ea5b3f
limda00 <dylim7715@gmail.com>
2024-09-04 22:20:23 +0900

    setting :: tailwind 디자인 시스템 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,206 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      gray900: '#6C6C6C',
+      gray800: '#848484',
+      gray700: '#9A9A9A',
+      gray600: '#A6A6A6',
+      gray500: '#BABABA',
+      gray400: '#CDCDCD',
+      gray300: '#D1D1D1',
+      gray200: '#EEEEEE',
+      gray100: '#FCFCFC',
+      main300: '#2079FF',
+      main200: '#1384EE',
+      main100: '#F6F8FE',
+      pointGreen: '#319A79',
+      green300: '#1BC48F',
+      green200: '#53CAA5',
+      green100: '#92CF93',
+      green50: '#9DD327',
+      pointPurple: '#8551DE',
+      purple300: '#596DFB',
+      purple200: '#6B7AF7',
+      purple100: '#D9DAFF',
+      pointBlue: '#00AEE0',
+      blue500: '#2E72B1',
+      blue400: '#4480B9',
+      blue300: '#5BA4E9',
+      blue200: '#84B9EA',
+      blue100: '#A7D1F9',
+      pointPink: '#D8426F',
+      pink200: '#C66683',
+      pink100: '#EAA0B6',
+      red100: '#FC5859',
+    },
+    fontSize: {
+      semi52: [
+        "52px",
+        {
+          fontFamily: "Pretendard-Semibold",
+          fontWeight: 600,
+          lineHeight: "54px"
+        }
+      ],
+      semi48: [
+        "48px",
+        {
+          fontWeight: 600,
+          lineHeight: "52px"
+        }
+      ],
+      bold40: [
+        "40px",
+        {
+          fontWeight: 700,
+          lineHeight: "48px"
+        }
+      ],
+      medium38: [
+        "38px",
+        {
+          fontWeight: 500,
+          lineHeight: "48px"
+        }
+      ],
+      semi36: [
+        "36px",
+        {
+          fontWeight: 600,
+          lineHeight: "48px"
+        }
+      ],
+      semi32: [
+        "32px",
+        {
+          fontWeight: 600,
+          lineHeight: "44px"
+        }
+      ],
+      bold24: [
+        "24px",
+        {
+          fontWeight: 700,
+          lineHeight: "36px"
+        }
+      ],
+      semi24: [
+        "24px",
+        {
+          fontWeight: 600,
+          lineHeight: "36px"
+        }
+      ],
+      medium24: [
+        "24px",
+        {
+          fontWeight: 500,
+          lineHeight: "36px"
+        }
+      ],
+      semi20: [
+        "20px",
+        {
+          fontWeight: 600,
+          lineHeight: "24px"
+        }
+      ],
+      medium20: [
+        "20px",
+        {
+          fontWeight: 500,
+          lineHeight: "24px"
+        }
+      ],
+      semi18: [
+        "18px",
+        {
+          fontWeight: 600,
+          lineHeight: "22px"
+        }
+      ],
+      medium18: [
+        "18px",
+        {
+          fontWeight: 500,
+          lineHeight: "22px"
+        }
+      ],
+      regular18: [
+        "18px",
+        {
+          fontWeight: 400,
+          lineHeight: "22px"
+        }
+      ],
+      bold16: [
+        "16px",
+        {
+          fontWeight: 700,
+          lineHeight: "20px"
+        }
+      ],
+      semi16: [
+        "16px",
+        {
+          fontWeight: 600,
+          lineHeight: "20px"
+        }
+      ],
+      medium16: [
+        "16px",
+        {
+          fontWeight: 500,
+          lineHeight: "20px"
+        }
+      ],
+      regular16: [
+        "16px",
+        {
+          fontWeight: 500,
+          lineHeight: "20px"
+        }
+      ],
+      semi14: [
+        "14px",
+        {
+          fontWeight: 600,
+          lineHeight: "18px"
+        }
+      ],
+      medium14: [
+        "14px",
+        {
+          fontWeight: 500,
+          lineHeight: "20px"
+        }
+      ],
+      regular14: [
+        "14px",
+        {
+          fontWeight: 400,
+          lineHeight: "24px"
+        }
+      ],
+      medium12: [
+        "12px",
+        {
+          fontWeight: 500,
+          lineHeight: "14px"
+        }
+      ],
+      regular12: [
+        "12px",
+        {
+          fontWeight: 400,
+          lineHeight: "14px"
+        }
+      ],
+    }
   },
   plugins: [],
-}
+};
