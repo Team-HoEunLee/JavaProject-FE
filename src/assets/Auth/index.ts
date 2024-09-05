0937f7e938b5552c053b4deee5becf96484da403
limda00 <dylim7715@gmail.com>
2024-09-05 18:45:23 +0900

    style :: Auth svg 파일 추가

---
+++ src/assets/Auth/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as Bag } from './Bag.svg';
+export { ReactComponent as Box } from './Box.svg';
+export { ReactComponent as Check } from './Check.svg';
+export { ReactComponent as Lock } from './Lock.svg';
+export { ReactComponent as Logo } from './Logo.svg';
+export { ReactComponent as User } from './User.svg';
