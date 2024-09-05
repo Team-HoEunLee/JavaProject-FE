b1b83dd03c49a58fd9ec5a04a9788da890d6c7aa
limda00 <dylim7715@gmail.com>
2024-09-05 20:59:31 +0900

    style :: svg 컴포넌트 추가

--- src/assets/Auth/index.ts
+++ src/assets/Auth/index.ts
@@ -1,6 +1,8 @@
+export { ReactComponent as ArrowDown } from './ArrowDown.svg';
 export { ReactComponent as Bag } from './Bag.svg';
 export { ReactComponent as Box } from './Box.svg';
 export { ReactComponent as Check } from './Check.svg';
 export { ReactComponent as Lock } from './Lock.svg';
 export { ReactComponent as Logo } from './Logo.svg';
 export { ReactComponent as User } from './User.svg';
+export { ReactComponent as Page } from './Page.svg';
