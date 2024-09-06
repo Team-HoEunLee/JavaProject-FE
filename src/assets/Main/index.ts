584a527559ba5bd7d1455714ba033b3146f70cf8
limda00 <dylim7715@gmail.com>
2024-09-06 22:19:51 +0900

    style :: 메인 svg 파일 추가

---
+++ src/assets/Main/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as Bulb } from './Bulb.svg';
+export { ReactComponent as HandShake } from './HandShake.svg';
+export { ReactComponent as Map } from './Map.svg';
+export { ReactComponent as Sprout } from './Sprout.svg';
+export { ReactComponent as SearchIcon } from './Search.svg';
+export { ReactComponent as Reset } from './Reset.svg';
