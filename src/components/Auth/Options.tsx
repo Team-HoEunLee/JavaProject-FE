9a22e8610b20c715f6f33348f35afc165c50a3a6
limda00 <dylim7715@gmail.com>
2024-09-06 12:55:17 +0900

    feat :: 옵션 클릭시 색 변경

--- src/components/Auth/Options.tsx
+++ src/components/Auth/Options.tsx
@@ -1,14 +1,22 @@
-import { Check } from '../../assets/Auth/index';
+import { useState } from 'react';
+import { Check, Checked } from '../../assets/Auth/index';
 
 interface PropsType {
   text?: string;
 }
 
 const Options = ({ text }: PropsType) => {
+  const [selected, setSelected] = useState<boolean>();
+
   return (
-    <div className="flex items-center px-5 py-2 gap-2 border-b border-b-gray400">
-      <Check />
-      <p className="w-full text-medium14 text-gray500">{text}</p>
+    <div
+      className="flex items-center px-5 py-2 gap-2 border-b border-b-gray400"
+      onClick={() => setSelected(!selected)}
+    >
+      {selected ? <Checked /> : <Check />}
+      <p className={`w-full text-medium14  ${selected ? 'text-main200' : 'text-gray500'}`}>
+        {text}
+      </p>
     </div>
   );
 };
