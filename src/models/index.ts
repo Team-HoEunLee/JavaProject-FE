bf29d3233857fe0663b31c805677b6a0940b176b
limda00 <dylim7715@gmail.com>
2024-09-06 22:23:33 +0900

    style :: 상수 추가 및 인터페이스 정의

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,35 @@ export interface AuthDataType {
   placeholder: string;
   icon: React.FC;
 }
+
+export interface MainItemsDataType {
+  name: string;
+  title: string;
+  info: string;
+  icon: React.FC;
+  backColor: string
+}
+
+export interface QuestionKeywordDataType {
+  name: string;
+  title: string;
+  keywords: Array<string>;
+}
+
+export interface QuestionListDataTypes {
+  checked: boolean;
+  title: string;
+  felid: Array<string>;
+  level: number;
+  avgScore: number;
+}
+
+export interface MainTableHeaderDataType {
+  width: string;
+  text: string;
+}
+
+export interface RankingListDataType {
+  name: string;
+  score: number;
+}
