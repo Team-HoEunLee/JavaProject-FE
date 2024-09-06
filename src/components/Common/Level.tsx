dda4dd6f4a5e9ab14f155baa135144f218f2cfd3
limda00 <dylim7715@gmail.com>
2024-09-06 22:25:06 +0900

    style :: 난이도 선택 컴포넌트 생성

---
+++ src/components/Common/Level.tsx
@@ -0,0 +1,15 @@
+import { Zero, One, Two, Three, Four } from '../../assets/Common/index';
+
+const Level = () => {
+  return (
+    <div className="flex gap-[12px]">
+      <Zero />
+      <One />
+      <Two />
+      <Three />
+      <Four />
+    </div>
+  );
+};
+
+export default Level;
