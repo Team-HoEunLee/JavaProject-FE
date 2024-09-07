a45ac9110c28ca592dbcf7b7200cf782aeea02ee
limda00 <dylim7715@gmail.com>
2024-09-07 22:46:53 +0900

    refactor :: 난이도 선택 컴포넌트 리팩토링

--- src/components/Common/Level.tsx
+++ src/components/Common/Level.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { Zero, One, Two, Three, Four } from '../../assets/Common/index';
 
 const Level = () => {
+  const images = [Zero, One, Two, Three, Four];
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const handleClick = (index: number) => {
+    console.log(index);
+    setSelectedIndex(index);
+  };
+
   return (
     <div className="flex gap-[12px]">
-      <Zero />
-      <One />
-      <Two />
-      <Three />
-      <Four />
+      {images.map((ImageComponent, index) => (
+        <ImageComponent
+          key={index}
+          onClick={() => handleClick(index)}
+          className={`rounded-full cursor-pointer ${selectedIndex === index ? 'border-[2px] border-white' : 'border-none'}`}
+        />
+      ))}
     </div>
   );
 };
