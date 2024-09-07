3fc5bce82f389ad65e11184f58c17023ed54b1b6
limda00 <dylim7715@gmail.com>
2024-09-08 08:54:31 +0900

    chore :: 랭킹 순위 번호 수정

--- src/components/Main/RankingList.tsx
+++ src/components/Main/RankingList.tsx
@@ -5,10 +5,11 @@ interface PropsType {
 }
 
 const RankingList = ({ number, name, score }: PropsType) => {
+  const rankingNumber = number != null ? number + 1 : undefined;
   return (
     <div className="flex justify-between py-[10px]">
       <div className="flex gap-2">
-        <p className="text-medium16 text-gray900">{`${number}위`}</p>
+        <p className="text-medium16 text-gray900">{`${rankingNumber}위`}</p>
         <p className="text-medium16">{name}</p>
       </div>
       <p className="text-medium16 text-main200">{`${score}점`}</p>
