61a222db31a9368d43dcd5e59b1b26afecf04764
limda00 <dylim7715@gmail.com>
2024-09-07 22:03:45 +0900

    feat :: 문제 검색창 값 저장

--- src/components/Main/Search.tsx
+++ src/components/Main/Search.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import { SearchIcon } from '../../assets/Main/index';
 
 const Search = () => {
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setSearchKeyword(e.target.value);
+  };
+
   return (
     <div className="w-full flex items-center gap-[8px] border border-gray400 rounded-md pl-[20px] overflow-hidden">
       <SearchIcon />
       <input
+        value={searchKeyword}
+        onChange={(e) => handelChange(e)}
         className="w-full py-[12px] text-medium16 outline-none"
         placeholder="문제 및 키워드를 검색해주세요."
       />
