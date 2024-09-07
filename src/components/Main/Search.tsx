524a1693037ef859cabe88f660c976bc66c99d77
limda00 <dylim7715@gmail.com>
2024-09-07 22:14:55 +0900

    feat :: 엔터키 누를시 입력 값 검색

--- src/components/Main/Search.tsx
+++ src/components/Main/Search.tsx
@@ -8,12 +8,23 @@ const Search = () => {
     setSearchKeyword(e.target.value);
   };
 
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      if (searchKeyword === '') {
+        return;
+      } else {
+        console.log(`${searchKeyword} 검색중`);
+      }
+    }
+  };
+
   return (
     <div className="w-full flex items-center gap-[8px] border border-gray400 rounded-md pl-[20px] overflow-hidden">
       <SearchIcon />
       <input
         value={searchKeyword}
         onChange={(e) => handelChange(e)}
+        onKeyDown={(e) => handleKeyDown(e)}
         className="w-full py-[12px] text-medium16 outline-none"
         placeholder="문제 및 키워드를 검색해주세요."
       />
