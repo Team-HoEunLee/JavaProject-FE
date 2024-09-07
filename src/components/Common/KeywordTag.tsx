82ccc06f9b5b8c3b03191d65f33258b7efb24fab
limda00 <dylim7715@gmail.com>
2024-09-07 20:35:07 +0900

    style :: 키워드 태그 컴포넌트 폰트 크기 수정

--- src/components/Common/KeywordTag.tsx
+++ src/components/Common/KeywordTag.tsx
@@ -5,7 +5,7 @@ interface PropsType {
 const KeywordTag = ({ text }: PropsType) => {
   return (
     <div className="px-[16px] py-[5px] border border-gray900 rounded-[20px]">
-      <p className="text-medium12 text-gray900">{text}</p>
+      <p className="text-medium14 text-gray900">{text}</p>
     </div>
   );
 };
