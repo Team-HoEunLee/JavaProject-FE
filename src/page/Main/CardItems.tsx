aaa8e9925ef57cbe8f5dfb4a89ddad80d9c89f9d
limda00 <dylim7715@gmail.com>
2024-09-08 08:48:55 +0900

    refactor :: 중복되는 코드 변수로 추출

--- src/page/Main/CardItems.tsx
+++ src/page/Main/CardItems.tsx
@@ -1,52 +1,57 @@
 import { useState } from 'react';
+import classNames from 'classnames';
 import { MainItemsCategory } from '../../constants/index';
 import { GoBack } from 'assets/Main';
 
 const CardItems = () => {
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
   const handleClick = (index: number) => {
     setSelectedIndex(index);
   };
 
   return (
     <div className="w-[1302px] flex gap-[34px]">
-      {MainItemsCategory.map((value, index) => (
-        <div
-          key={index}
-          onClick={() => handleClick(index)}
-          className={`relative flex flex-col justify-between h-[280px] pt-[32px] pl-[25px] pb-[10px] pr-[10px] rounded-xl cursor-pointer transition-all duration-500 ease-in-out
-          ${selectedIndex === index ? 'w-[1302px]' : 'w-[300px]'}
-          ${selectedIndex === index ? 'scale-105' : 'scale-100'}
-          ${selectedIndex !== index && selectedIndex !== null ? 'hidden' : 'flex'}
-          ${value.backColor}`}
-        >
-          <div className="flex flex-col gap-2">
-            <p
-              className={`text-bold24 text-white 
-              ${selectedIndex === index ? 'w-full' : 'w-[140px]'}`}
-            >
-              {value.title}
-            </p>
-            <p
-              className={`text-semi16 text-white 
-              ${selectedIndex === index ? 'flex' : 'hidden'}`}
-            >
-              {value.info}
-            </p>
-          </div>
-          <a
-            href="/main"
-            className={`flex items-center decoration-white hover:underline
-            ${selectedIndex === index ? 'flex' : 'hidden'}`}
+      {MainItemsCategory.map((value, index) => {
+        const isSelected = selectedIndex === index;
+        return (
+          <div
+            key={index}
+            onClick={() => handleClick(index)}
+            className={classNames(
+              'relative flex flex-col justify-between h-[280px] pt-[32px] pl-[25px] pb-[10px] pr-[10px] rounded-xl cursor-pointer transition-all duration-500 ease-in-out',
+              {
+                'w-[1302px] scale-105': isSelected,
+                'w-[300px] scale-100': !isSelected,
+                hidden: !isSelected && selectedIndex !== null,
+                flex: !(!isSelected && selectedIndex !== null),
+              },
+              value.backColor,
+            )}
           >
-            <GoBack />
-            <p className="text-semi16 text-white">홈으로 돌아가기</p>
-          </a>
-          <div className="absolute right-[10px] bottom-[10px]">
-            <value.icon />
+            <div className="flex flex-col gap-2">
+              <p
+                className={classNames('text-bold24 text-white', {
+                  'w-full': isSelected,
+                  'w-[140px]': !isSelected,
+                })}
+              >
+                {value.title}
+              </p>
+              {isSelected && <p className="text-semi16 text-white">{value.info}</p>}
+            </div>
+            {isSelected && (
+              <a href="/main" className="flex items-center decoration-white hover:underline">
+                <GoBack />
+                <p className="text-semi16 text-white">홈으로 돌아가기</p>
+              </a>
+            )}
+            <div className="absolute right-[10px] bottom-[10px]">
+              <value.icon />
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
