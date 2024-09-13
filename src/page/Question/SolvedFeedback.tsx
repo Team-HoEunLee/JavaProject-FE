4e70627ef74680132704acd04265fdc9aa133fa5
limda00 <dylim7715@gmail.com>
2024-09-13 11:05:08 +0900

    style :: 피드백 페이지 폰트 스타일 변경

--- src/page/Question/SolvedFeedback.tsx
+++ src/page/Question/SolvedFeedback.tsx
@@ -6,13 +6,13 @@ const SolvedFeedback = () => {
           <div className="flex flex-col gap-[8px]">
             <p className="text-semi20">풀이한 문제</p>
             <div className="py-[8px] px-[20px] border-l-[2px]">
-              <p className="text-medium16 text-gray900">HTTP와 HTTPS의 차이점에 대해 서술하시오</p>
+              <p className="text-medium16">HTTP와 HTTPS의 차이점에 대해 서술하시오</p>
             </div>
           </div>
           <div className="flex flex-col gap-[8px]">
             <p className="text-semi20">나의 풀이</p>
-            <div className="py-[32px] px-[24px] border border-gray400 rounded-[8px]">
-              <p className="text-medium16 text-gray900 leading-[180%]">
+            <div className="py-[24px] px-[20px] border border-gray300 rounded-[8px]">
+              <p className="text-regular14 text-gray900 leading-[220%]">
                 HTTP는 Hyper Text Transfer Protocol의 약자로 HTML 문서나 JSON 데이터를 클라이언트와
                 서버에서 주고 받을 수 있도록 만든 통신 규약이다 HTTPS는 HTTP를 보안상으로 더
                 강화시킨 것으로 신뢰성이 보장되고 더 안전하게 데이터를 주고 받을 수 있도록 만든다
@@ -21,8 +21,8 @@ const SolvedFeedback = () => {
           </div>
           <div className="flex flex-col gap-[8px]">
             <p className="text-semi20">AI 피드백</p>
-            <div className="py-[32px] px-[24px] border border-gray400 rounded-[8px]">
-              <p className="text-medium16 text-gray900 leading-[180%]">
+            <div className="py-[24px] px-[20px] border border-gray300 rounded-[8px]">
+              <p className="text-regular14 text-gray900 leading-[220%]">
                 답변은 기본적인 차이점을 잘 설명하고 있으나, 좀 더 구체적인 정보를 추가하면
                 좋겠습니다. 다음과 같은 사항을 포함할 수 있습니다: 암호화: HTTPS는 SSL/TLS(보안 소켓
                 계층/전송 계층 보안)를 사용하여 데이터 전송을 암호화합니다. 이로 인해 데이터가 전송
@@ -39,7 +39,7 @@ const SolvedFeedback = () => {
         </div>
         <div className="w-[280px] flex flex-col p-[20px] gap-[32px]">
           <div className="flex flex-col py-[4px] gap-[40px] border-b border-gray400">
-            <p className="text-medium16">임다영 님</p>
+            <p className="text-semi16">임다영 님</p>
           </div>
           <div className="flex flex-col gap-[12px]">
             <div className="flex flex-col gap-2">
@@ -50,9 +50,13 @@ const SolvedFeedback = () => {
               총 <span className="text-main300">20점</span>을 획득하였습니다
             </p>
           </div>
-          <div className="flex flex-col px-[12px] py-[20px] bg-main100">
-            <p className="text-medium14 text-gray800">이 문제를 20000명이 풀이하였습니다</p>
-            <p className="text-medium14 text-gray800">사용자의 평균 점수는 40점 입니다</p>
+          <div className="flex flex-col px-[12px] py-[20px] rounded-[8px] bg-main100">
+            <p className="text-medium14 text-gray700">
+              이 문제를 <span className="text-gray900">20000명</span>이 풀이하였습니다
+            </p>
+            <p className="text-medium14 text-gray700">
+              사용자의 평균 점수는 <span className="text-gray900">40점</span>입니다
+            </p>
           </div>
         </div>
       </div>
