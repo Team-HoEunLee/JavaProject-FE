82aa2fcff464d31fc5f6b94dc0087db2a97c962b
limda00 <dylim7715@gmail.com>
2024-10-18 21:52:58 +0900

    style :: 문제 등록 페이지 퍼블리싱

---
+++ src/page/Admin/WriteQuestion.tsx
@@ -0,0 +1,70 @@
+import SubmitButton from 'components/Common/SubmitButton';
+import KeywordTag from 'components/Common/KeywordTag';
+import { Major } from '../../constants';
+import getLevelIcon from 'utils/getLevelIcon';
+
+const WriteQuestion = () => {
+  const handleClickSubmit = () => {
+    console.log('문제를 등록하였습니다');
+  };
+
+  return (
+    <div className="w-full h-screen flex justify-center">
+      <div className="w-[1302px] h-[80%] flex justify-between gap-[20px] bg-red">
+        <div className="flex flex-col justify-between">
+          <div className="w-[390px] flex flex-col gap-[24px]">
+            <div className="flex flex-col gap-[4px]">
+              <p className="text-semi20">
+                <span className="text-main300">I</span>nterview
+                <span className="text-main300"> Q</span>uestion
+                <span className="text-main300"> C</span>olumn
+              </p>
+              <p className="text-medium14">면접 질문과 관련된 항목을 체크해 주세요</p>
+            </div>
+            <div className="flex flex-col gap-[20px] py-[20px] px-[16px] border border-gray200 rounded-[8px] bg-gray100">
+              <p className="text-medium14">카테고리 및 분야</p>
+              <div className="flex flex-wrap gap-[8px]">
+                {Major.map((value, index) => (
+                  <KeywordTag key={index} text={value} />
+                ))}
+              </div>
+              <div className="flex justify-between gap-[20px]">
+                <input
+                  className="flex-1 border border-gray300 py-[8px] px-[12px] rounded-[20px] placeholder:text-regular14"
+                  placeholder="이 외 세부 기술스택을 추가해보세요"
+                />
+                <button className="w-[55px] px-[8px] py-[4px] rounded-[20px] bg-main300">
+                  <p className="text-white">추가</p>
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-col gap-[4px] py-[20px] px-[16px] border border-gray200 rounded-[8px] bg-gray100">
+              <p className="text-medium14">난이도</p>
+              <div className="w-full flex justify-center gap-[28px]">
+                {[0, 1, 2, 3, 4].map((value) => getLevelIcon(value))}
+              </div>
+            </div>
+          </div>
+          <SubmitButton text="문제 등록하기" onClick={() => handleClickSubmit()} />
+        </div>
+        <div className="w-full h-full flex flex-col gap-[12px]">
+          <p className="text-semi20">
+            <span className="text-main300">면접 질문</span>을 작성해 주세요
+          </p>
+          <div className="h-full py-[20px] px-[30px] flex flex-col bg-gray100 border border-gray200 rounded-[8px] gap-[20px]">
+            <input
+              placeholder="제목을 작성해주세요"
+              className="w-full border-b border-blue100 py-[8px]"
+            />
+            <textarea
+              placeholder="문제를 작성해주세요"
+              className="h-full px-[12px] py-[8px] bg-white border border-blue100 rounded-[8px] resize-none outline-none"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WriteQuestion;
