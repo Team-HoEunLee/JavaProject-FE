72bda364f0cb32dbb0881d9476211a49ae4cbaac
limda00 <dylim7715@gmail.com>
2024-10-18 21:52:12 +0900

    style :: 카테고리 문제 리스트 더미 값 추가

---
+++ src/components/dummy/categoryList.ts
@@ -0,0 +1,40 @@
+/**
+ * 카테고리 조회
+ */
+
+export const category = {
+  "codes": [
+    {
+      "id": 0,
+      "codeName": "string"
+    }
+  ],
+  "areas": [
+    {
+      "id": 0,
+      "areaName": "string",
+      "quiz": {
+        "id": 0,
+        "title": "string",
+        "question": "string",
+        "level": "EASY",
+        "code": {
+          "id": 0,
+          "codeName": "string"
+        },
+        "area": "string",
+        "user": {
+          "id": 0,
+          "accountId": "string",
+          "email": "string",
+          "password": "string",
+          "name": "string",
+          "introduction": "string",
+          "area": "string",
+          "role": "STUDENT",
+          "score": 0
+        }
+      }
+    }
+  ]
+}
---
+++ src/components/dummy/questionList.ts
@@ -0,0 +1,38 @@
+/**
+ * 문제 리스트 조회
+ */
+
+export const questionList = [
+  {
+    "quizId": 0,
+    "title": "string",
+    "question": "string",
+    "area": {
+      "id": 0,
+      "areaName": "string",
+      "quiz": {
+        "id": 0,
+        "title": "string",
+        "question": "string",
+        "level": "EASY",
+        "code": {
+          "id": 0,
+          "codeName": "string"
+        },
+        "area": "string",
+        "user": {
+          "id": 0,
+          "accountId": "string",
+          "email": "string",
+          "password": "string",
+          "name": "string",
+          "introduction": "string",
+          "area": "string",
+          "role": "STUDENT",
+          "score": 0
+        }
+      }
+    },
+    "level": "EASY"
+  }
+]
