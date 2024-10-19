e92933c8152412552f21d7ba7b6708781368f3f4
limda00 <dylim7715@gmail.com>
2024-10-19 23:21:57 +0900

    style :: 헤더 어드민 조건 추가

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -4,7 +4,9 @@ import { Profile } from '../../assets/Main';
 import Button from 'components/Auth/Button';
 
 const Header = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  //나중에 고쳐야 됨 (로그인 시 / 어드민일 경우 조건)
+  const [isLogin, setIsLogin] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
   return (
     <div className="flex justify-center p-[12px]">
       <div className="w-[1302px] flex justify-between">
@@ -12,8 +14,8 @@ const Header = () => {
         {!isLogin ? (
           <Button text="로그인" />
         ) : (
-          <div className="flex gap-[8px]">
-            <Button text="문제 추가" />
+          <div className="flex gap-[20px]">
+            {isAdmin && <Button text="문제 추가" />}
             <div className="flex items-center gap-[8px]">
               <Profile />
               <p className="text-medium16 text-gray800">홍길동님</p>
