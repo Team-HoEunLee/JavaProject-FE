5498403e1f3b1075dc718e8f5dd35b99dfae8570
Tyler <tylereunholee010203@gmail.com>
2024-10-22 16:26:07 +0900

    fix :: conflict 해결

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,6 +6,8 @@ import Main from '../page/Main/UserMain';
 import QuestionSolving from 'page/Question/QuestionSolving';
 import SolvedFeedback from 'page/Question/SolvedFeedback';
 import MyPage from 'page/User/MyPage';
+import WriteQuestion from 'page/Admin/WriteQuestion';
+import Layout from 'components/Layout';
 
 const Router = () => {
   return (
@@ -17,7 +19,13 @@ const Router = () => {
         <Route path="/main" element={<Main />} />
         <Route path="/solve" element={<QuestionSolving />} />
         <Route path="/feedback" element={<SolvedFeedback />} />
+        <Route path="/writeQuestion" element={<WriteQuestion />} />
         <Route path="/user" element={<MyPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/main" element={<Main />} />
+          <Route path="/solve" element={<QuestionSolving />} />
+          <Route path="/feedback" element={<SolvedFeedback />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
