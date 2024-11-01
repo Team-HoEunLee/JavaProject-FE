771bbf3f43527a21d068294d1a802699bcce08b1
limda00 <dylim7715@gmail.com>
2024-11-01 10:49:07 +0900

    feat :: 인스턴스 생성

---
+++ src/utils/api/axios/index.ts
@@ -0,0 +1,63 @@
+import axios, { AxiosError } from "axios"
+import { getToken, setToken } from "../function/TokenManage"
+
+const BASEURL = process.env.REACT_APP_BASE_URL
+
+export const instance = axios.create({
+  baseURL: BASEURL,
+  timeout: 10000,
+});
+
+export const refreshInstance = axios.create({
+  baseURL: BASEURL,
+  timeout: 10000
+})
+
+instance.interceptors.request.use(
+  async function(config) {
+    const { accessToken } = getToken()
+
+    if(accessToken) {
+      config.headers["authorization"] = `Bearer ${accessToken}`
+    }
+    return config
+  },
+  function (error: AxiosError) {
+    console.error("Request error:", error);
+    return Promise.reject(error);
+  }
+)
+
+instance.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  async function(error: AxiosError) {
+    if(axios.isAxiosError(error) && error.response) {
+      const { status } = error.response
+      
+      if(status === 401) {
+        const refreshToken = getToken().refreshToken
+        try {
+          await axios.get(`http://${BASEURL}/auth/refresh`,{
+            headers: {
+              "authorization": `Bearer ${refreshToken}`
+            }
+          })
+          .then((response) => {
+            const data = response.data
+            setToken(data.access_token, data.refresh_token)
+          })
+          .catch(() => {
+            window.location.href = "login";
+          })
+        } catch(refreshError) {
+          return Promise.reject(refreshError)
+        }
+      }
+    }
+    return Promise.reject(error)
+  } 
+)
+
+export default instance;
