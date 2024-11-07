cfa5d3055c64ea98e88d7fef947528da26e61c37
limda00 <dylim7715@gmail.com>
2024-11-07 17:15:11 +0900

    style :: 코드 Eslint

--- src/components/dummy/questionList.ts
+++ src/components/dummy/questionList.ts
@@ -4,35 +4,35 @@
 
 export const questionList = [
   {
-    "quizId": 0,
-    "title": "string",
-    "question": "string",
-    "area": {
-      "id": 0,
-      "areaName": "string",
-      "quiz": {
-        "id": 0,
-        "title": "string",
-        "question": "string",
-        "level": "EASY",
-        "code": {
-          "id": 0,
-          "codeName": "string"
+    quizId: 0,
+    title: 'string',
+    question: 'string',
+    area: {
+      id: 0,
+      areaName: 'string',
+      quiz: {
+        id: 0,
+        title: 'string',
+        question: 'string',
+        level: 'EASY',
+        code: {
+          id: 0,
+          codeName: 'string',
         },
-        "area": "string",
-        "user": {
-          "id": 0,
-          "accountId": "string",
-          "email": "string",
-          "password": "string",
-          "name": "string",
-          "introduction": "string",
-          "area": "string",
-          "role": "STUDENT",
-          "score": 0
-        }
-      }
+        area: 'string',
+        user: {
+          id: 0,
+          accountId: 'string',
+          email: 'string',
+          password: 'string',
+          name: 'string',
+          introduction: 'string',
+          area: 'string',
+          role: 'STUDENT',
+          score: 0,
+        },
+      },
     },
-    "level": "EASY"
-  }
-]
+    level: 'EASY',
+  },
+];
--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,18 +1,18 @@
 export interface TokenResponse {
-  accessToken: string,
-  refreshToken: string
+  accessToken: string;
+  refreshToken: string;
 }
 
 export interface SignupRequest {
-  accountId: string,
-  email: string,
-  password: string,
-  name: string,
-  introduction: string,
-  areaId: number
+  accountId: string;
+  email: string;
+  password: string;
+  name: string;
+  introduction: string;
+  areaId: number;
 }
 
 export interface LoginRequest {
-  accountId: string,
-  password: string
+  accountId: string;
+  password: string;
 }
